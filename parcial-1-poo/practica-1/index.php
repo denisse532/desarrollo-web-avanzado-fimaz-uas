2cd88c366a65e080ba16f44ddffdb5f85f446672
denisse532 <denissega908@gmail.com>
2026-03-10 16:54:50 -0700

    index.php

--- parcial-1-poo/practica-1/index.php
+++ parcial-1-poo/practica-1/index.php
@@ -3,7 +3,7 @@
 require_once 'Usuario.php';
 $objusuario = new Usuario(" denisse"," denisse@gmail.com");
 
-echo "Nombre:" . $usuario->getNombre(). "<br>";
-echo "Correo:" . $usuario->getCorreo();
+echo "Nombre:" . $objusuario->getNombre(). "<br>";
+echo "Correo:" . $objusuario->getCorreo();
 =======
 >>>>>>> 3d888fe (Actualizar archivos practica 1)
