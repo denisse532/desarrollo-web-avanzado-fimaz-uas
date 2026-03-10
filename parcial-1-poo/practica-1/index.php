851de18fe2fb65ec08a719fce7338c4693406a5b
denisse532 <denissega908@gmail.com>
2026-03-10 16:49:44 -0700

    index.php

--- parcial-1-poo/practica-1/index.php
+++ parcial-1-poo/practica-1/index.php
@@ -1,7 +1,7 @@
 <<<<<<< HEAD
 <?php
 require_once 'Usuario.php';
-$usuario = new Usuario(" denisse"," denisse@gmail.com");
+$objusuario = new Usuario(" denisse"," denisse@gmail.com");
 
 echo "Nombre:" . $usuario->getNombre(). "<br>";
 echo "Correo:" . $usuario->getCorreo();
